e06f673b1b38f06c4634ffa6e763fcc881c627f4
Amir-kali-linux <amirevercode@gmail.com>
2025-02-07 00:04:46 +0900

    Add files via upload

---
+++ filtrinrg for rubika_id.py
@@ -0,0 +1,37 @@
+﻿#github -----> https://github.com/amir-kali-linux/
+import random
+import time
+print('----------------------------------')
+print('--->(cood filtring for rubika)<---')
+print('----------------------------------')
+print('https://github.com/amir-kali-linux/')
+print('')
+time.sleep(6)
+print('1: cood')
+print('2: cood for virusys')
+print('3: virusys')
+print('4: vpn')
+print('5: cood hangey')
+choose = input('SUMMARY--->|')
+time.sleep(4)
+
+if (str(choose) == '1'):
+    print('https://el.phncdn.com/pics/gifs/050/586/631/(m=bKOCwLV)(mh=Ojej82L8R9WiVs1-)50586631a.jpg')
+elif (str(choose) == '3'):
+    random.seed(5)
+    print(random.random())
+
+if (str(choose) == '2'):
+    print('https://el.phncdn.com/pics/gifs/050/586/631/(m=bKOCwLV)(mh=Ojej82L8R9WiVs1-)50586631a.jpg./(virusys)./8.8.8.8/google/filtring/rubika/sex.sex.sex.sex.sex/vpn/vip/xxx/www.pornhub.com/fil(1.fil(2.fil(3.fil(4./gifs/sex/sex/sex/sex.com')
+elif (str(choose) == '0'):
+    print('')
+
+if (str(choose) == '4'):
+    print('s3.amazonaws.com/(vpn./sex(google)filter)./050.8.8.8.8/999/1/3/4/.fil/0./(‡◌ﭐ۩۞۝d۸۷۸۝۳…ﭬ۩٦)')
+elif (str(choose) == '0'):
+    print('')
+
+if (str(choose) == '5'):
+    print('۝’“سو۷••‘‘’–۞‘‘‘’’۝‚ጃጃጆፄ፠፠፠፼፼፻ᎂᎈᎋᎋᎍⶻⶻⶻ®acc旔旎昉昨晈晡晟晝斤斣斬斐斎敫ﾅﾃﾗ９ｹﾍ￥ﾑｽ·ñﷴ﷼ﳳ﷽ﺵ«èèëëìíîîïðAa¥&$A@? ~}|?????????????????????????~~Wdx*B?r_³¶«èèëëìíîîïðAa¥&$A@? ~}|?????????????????????????~~Wdx*B?r_³¶§§§§§¸ÏÍÎÚÖÖ×ÂÀ¿?¯^```???``````````````````````````````````?????????????¸¸¸???t?????‰†ﮕﮔ')
+elif (str(choose) == '0'):
+    print('')
